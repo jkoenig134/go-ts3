970c0634c5c6673b209bf286adad99ee2c04a42e
Julian König <julian.koenig@js-soft.com>
2020-05-06 01:20:04 +0200

    refactor: move event

--- client.go
+++ client.go
@@ -91,29 +91,3 @@ func (c *TeamspeakHttpClient) request(path string) (*[]byte, error) {
 func vServerUrl(id int, path string) string {
 	return fmt.Sprintf("%d/%s", id, path)
 }
-
-type TeamspeakEvent string
-
-//noinspection GoUnusedConst
-const (
-	TextMessage               = "notifytextmessage"
-	ClientEnterView           = "notifycliententerview"
-	ClientLeftView            = "notifyclientleftview"
-	ServerEdited              = "notifyserveredited"
-	ChannelEdited             = "notifychanneledited"
-	ChannelDescriptionChanged = "notifychanneldescriptionchanged"
-	ClientMoved               = "notifyclientmoved"
-	ChannelCreated            = "notifychannelcreated"
-	ChannelDeleted            = "notifychanneldeleted"
-	ChannelMoved              = "notifychannelmoved"
-	ChannelPasswordChanged    = "notifychannelpasswordchanged"
-	TokenUsed                 = "notifytokenused"
-)
-
-func (c *TeamspeakHttpClient) SubscribeEvent(event TeamspeakEvent, fn interface{}) error {
-	return c.eventBus.Subscribe(string(event), fn)
-}
-
-func (c *TeamspeakHttpClient) UnsubscribeEvent(event TeamspeakEvent, handler interface{}) error {
-	return c.eventBus.Unsubscribe(string(event), handler)
-}
